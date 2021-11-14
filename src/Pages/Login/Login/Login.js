d3f0ae49c454f9b93227be05cd584668fa0eb9b9
mdsajedul <sajedulislms@gmail.com>
2021-11-14 22:13:06 +0600

    some changed in navigation logout done

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link,useLocation,useHistory } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
 import './login.css';
 
 
 const Login = () => {
     const { register, handleSubmit,reset } = useForm();
+    const {userLogin} = useAuth()
+
+    const location = useLocation();
+    const history = useHistory();
+
+    const onSubmit =(data)=>{
+        console.log(data)
+        userLogin(data.email,data.password, location, history)
+    }
     return (
         <div className="d-flex justify-content-center p-5">
             <div className="w-75">
             <i class="fas fa-user"></i>
                 <h3 style={{color:'#1565C0'}}>Login</h3>
-                <form onSubmit="">
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div>
                         <input type="text" {...register("email", { required: true })}  placeholder= " Your Email" />
                     </div>
--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
+import useAuth from '../../../hooks/useAuth';
 import logo from '../../../images/logo/logo.png'
 
+
 const Navigation = () => {
+    const {user,logout} = useAuth();
     return (
         <>
             <Navbar sticky="top" className="nav-custom" collapseOnSelect expand="lg" bg="light" variant="light">
@@ -23,7 +26,18 @@ const Navigation = () => {
                         <Nav.Link as={HashLink} to="/home#explore">Explore</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            {user.email && 
+                            <div style={{display:'inline-flex'}}>
+                                <Nav.Link  as={HashLink} to="/dashboard">Dashboard</Nav.Link>
+                                <Navbar.Text style={{paddingRight:'5px',paddingLeft:'5px'}} className="disply-name">
+                                    Hello,<span className="user-name">{user?.displayName}</span>
+                                </Navbar.Text>
+                            </div>
+                             
+                            }
+                            {user?.email ?
+                                <button className="btn-logout" style={{border:'none',color:'red', backgroundColor:'#F8F9FA'}} onClick={logout} variant="light">Logout</button> :
+                                <Nav.Link as={Link} to="/login">Login</Nav.Link>}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
